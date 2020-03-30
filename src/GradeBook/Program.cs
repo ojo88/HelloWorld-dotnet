4783abad94ee1050e167be12128928ab57c560c6
Joseph Okpara <joejr.okpara@gmail.com>
2020-03-30 17:08:18 -0500

    learned a bit about collections in csharp and was introduced to List<T> class

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,6 +7,24 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+           /* var x = 34.1;  //like javascript, i can use var. var is an implicit data type and is defiend by the data stored in the vairable.  
+            var y = 10.3;  //Basically it tells the compiler to figure out the type by what's stored in the variable at compilation. x and y become double 
+            var result = x + y;
+            */
+
+            //double[] numbers = new double[3];
+            //var numbers = new[] {12.7, 10.3, 6.11, 4.1};
+            //List<double> grades = new List<double>();
+            var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
+            grades.Add(56.1);
+            
+
+            var result = 0.0;
+            foreach(double number in grades){
+                result +=number;
+            }
+            result = result/grades.Count;
+            System.Console.WriteLine($"The average grade is {result:N1}");
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
